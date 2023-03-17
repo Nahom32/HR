2bb8aaa683b2813e48fc37a2f94eb7fabe0e10bb
Nahom32 <senaynahom00@gmail.com>
2023-03-17 13:00:05 +0300

    Started The Employee Service

---
+++ Human Resources/Human Resources/Data/Services/EmployeeService.cs
@@ -0,0 +1,38 @@
+﻿using Human_Resources.Models;
+
+namespace Human_Resources.Data.Services
+{
+    public class EmployeeService : IEmployeeService
+    {
+        private readonly AppDbContext _context;
+
+        public EmployeeService(AppDbContext context)
+        {
+            _context = context;  
+        }
+        public Task AddEmployee(Employee employee)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteDepartment(Employee employee)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Employee>> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Employee> GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateDepartment(Employee employee)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Human Resources/Human Resources/Data/Services/IEmployeeService.cs
@@ -0,0 +1,13 @@
+﻿using Human_Resources.Models;
+
+namespace Human_Resources.Data.Services
+{
+    public interface IEmployeeService
+    {
+        public Task<List<Employee>> GetAll();
+        public Task<Employee> GetById(int id);
+        public Task AddEmployee(Employee employee);
+        public void UpdateDepartment(Employee employee);
+        public void DeleteDepartment(Employee employee);
+    }
+}
